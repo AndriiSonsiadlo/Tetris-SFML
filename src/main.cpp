6e05bfab83dbebaaa165b2d5ad071d9a2a3fd884
Andrii Sonsiadlo <andrii.sonsiadlo@gmail.com>
2024-08-03 14:48:12 +0200

    Implement basic SFML window and event handling in main function

--- src/main.cpp
+++ src/main.cpp
@@ -4,5 +4,26 @@
 
 int main()
 {
+    std::cout << "SFML Version: " << SFML_VERSION_MAJOR << "."
+        << SFML_VERSION_MINOR << "."
+        << SFML_VERSION_PATCH << std::endl;
+
+    auto window = sf::RenderWindow(sf::VideoMode({800, 400}), "CMake SFML Project");
+    window.setFramerateLimit(165);
+
+    while (window.isOpen())
+    {
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+            {
+                window.close();
+            }
+        }
+
+        window.clear(sf::Color::Blue);
+
+        window.display();
+    }
     return 0;
 }
