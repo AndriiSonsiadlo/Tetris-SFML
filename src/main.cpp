5e1a553e7a45e49190a6eb53ed1f85389f62d7b9
Andrii Sonsiadlo <andrii.sonsiadlo@gmail.com>
2024-08-04 13:25:54 +0200

    draft: add shape rendering and texture loading in main function

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
-#include <iostream>
 #include <SFML/Graphics.hpp>
+#include <iostream>
+
+#define ASSETS_DIR "../assets/"
 
 
 int main()
@@ -22,6 +24,73 @@ int main()
         }
 
         window.clear(sf::Color::Blue);
+
+        sf::CircleShape circle(60.f, 30);
+        circle.setFillColor(sf::Color(230, 0, 50));
+        circle.setOutlineThickness(5.f);
+        circle.setOutlineColor(sf::Color(80, 220, 50));
+        circle.setPosition({15, 15});
+        window.draw(circle);
+
+        sf::CircleShape triangle(60.f, 3);
+        triangle.setPosition(circle.getPosition() + sf::Vector2f{150, 0});
+        triangle.setFillColor(sf::Color::Yellow);
+        window.draw(triangle);
+
+        sf::CircleShape square(60.f, 4);
+        square.setPosition(triangle.getPosition() + sf::Vector2f{150, 0});
+        square.setFillColor(sf::Color::Red);
+        window.draw(square);
+
+        sf::CircleShape octagon(60.f, 8);
+        octagon.setPosition(square.getPosition() + sf::Vector2f{150, 0});
+        octagon.setFillColor(sf::Color::Cyan);
+        window.draw(octagon);
+
+        sf::ConvexShape convex;
+        convex.setPointCount(5);
+
+        convex.setPoint(0, sf::Vector2f(0.f, 0.f));
+        convex.setPoint(1, sf::Vector2f(150.f, 10.f));
+        convex.setPoint(2, sf::Vector2f(120.f, 90.f));
+        convex.setPoint(3, sf::Vector2f(30.f, 100.f));
+        convex.setPoint(4, sf::Vector2f(5.f, 50.f));
+
+        convex.setFillColor(sf::Color::Black);
+        convex.move({1, 150});
+        window.draw(convex);
+
+        sf::RectangleShape rectangle(sf::Vector2f(70.f, 100.f));
+        rectangle.move({165, 150});
+        rectangle.setFillColor(sf::Color(175, 180, 240));
+        window.draw(rectangle);
+
+        sf::RectangleShape line_with_thickness(sf::Vector2f(130.f, 5.f));
+        line_with_thickness.rotate(sf::degrees(45.f));
+        line_with_thickness.setFillColor(sf::Color(15, 180, 140));
+        line_with_thickness.move({250, 150});
+        window.draw(line_with_thickness);
+
+        sf::Vertex line_without_thickness[] = {
+            sf::Vertex(sf::Vector2f(390.f, 240.f)),
+            sf::Vertex(sf::Vector2f(470.f, 150.f))
+        };
+        line_without_thickness->color = sf::Color::Black;
+        window.draw(line_without_thickness, 2, sf::PrimitiveType::Lines);
+
+        sf::Texture texture;
+        if (const bool textureIsLoaded = texture.loadFromFile(
+                ASSETS_DIR "texture.png", false,
+                sf::IntRect(sf::Vector2{50, 50}, sf::Vector2{100, 100}));
+            !textureIsLoaded)
+        {
+            std::cerr << "Failed to load texture!" << std::endl;
+            return 1;
+        }
+        sf::Sprite sprite(texture);
+        sprite.setPosition({10, 300});
+
+        window.draw(sprite);
 
         window.display();
     }
