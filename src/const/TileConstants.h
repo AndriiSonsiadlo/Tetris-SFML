dde0e10df1ce12705f70687db9630cb027698ba4
Andrii Sonsiadlo <andrii.sonsiadlo@gmail.com>
2024-08-05 17:21:12 +0200

    feat: add TileConstants with tile size, colors, and pre-defined tile rects

---
+++ src/const/TileConstants.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+
+
+namespace Tetris::TileConstants
+{
+    constexpr int TILE_SIZE = 30;
+    constexpr int TILES_COUNT = 8;
+
+    enum class TileColor
+    {
+        CYAN = 0,
+        BLUE = 1,
+        PURPLE = 2,
+        PINK = 3,
+        LIGHT_GREEN = 4,
+        YELLOW = 5,
+        RED = 6,
+        GREEN = 7
+    };
+
+    constexpr sf::IntRect TILE_CYAN({0, 0}, {TILE_SIZE, TILE_SIZE});
+    constexpr sf::IntRect TILE_BLUE({30, 0}, {TILE_SIZE, TILE_SIZE});
+    constexpr sf::IntRect TILE_PURPLE({60, 0}, {TILE_SIZE, TILE_SIZE});
+    constexpr sf::IntRect TILE_PINK({90, 0}, {TILE_SIZE, TILE_SIZE});
+    constexpr sf::IntRect TILE_LIGHT_GREEN({120, 0}, {TILE_SIZE, TILE_SIZE});
+    constexpr sf::IntRect TILE_YELLOW({150, 0}, {TILE_SIZE, TILE_SIZE});
+    constexpr sf::IntRect TILE_RED({180, 0}, {TILE_SIZE, TILE_SIZE});
+    constexpr sf::IntRect TILE_GREEN({210, 0}, {TILE_SIZE, TILE_SIZE});
+
+    constexpr sf::IntRect TILE_RECTS[TILES_COUNT] = {
+        TILE_CYAN, TILE_BLUE, TILE_PURPLE, TILE_PINK,
+        TILE_LIGHT_GREEN, TILE_YELLOW, TILE_RED, TILE_GREEN
+    };
+}
