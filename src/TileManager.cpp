6a7af2efdf9b7a6cce99a6da0ab05145d4416514
Andrii Sonsiadlo <andrii.sonsiadlo@gmail.com>
2024-08-05 18:05:42 +0200

    feat: implement TileManager for managing tile sprites

--- src/TileManager.cpp
+++ src/TileManager.cpp
@@ -2,3 +2,40 @@
 
 #include "TileManager.h"
 
+
+TileManager::TileManager()
+{
+}
+
+bool TileManager::loadTileTexture(const std::string& filename)
+{
+    if (!tileTexture.loadFromFile(filename))
+    {
+        std::cerr << "Error loading tile texture: " << filename << std::endl;
+        return false;
+    }
+    return true;
+}
+
+const sf::Texture& TileManager::getTexture() const
+{
+    return tileTexture;
+}
+
+
+sf::IntRect TileManager::getTileRect(Tetris::TileConstants::TileColor color)
+{
+    const int tileIndex = static_cast<int>(color);
+    return Tetris::TileConstants::TILE_RECTS[tileIndex];
+}
+
+void TileManager::setSpriteTile(sf::Sprite& sprite, const Tetris::TileConstants::TileColor color) const
+{
+    sprite.setTexture(tileTexture);
+    sprite.setTextureRect(getTileRect(color));
+}
+
+sf::Sprite TileManager::createSprite(const Tetris::TileConstants::TileColor color) const
+{
+    return sf::Sprite(tileTexture, getTileRect(color));
+}
