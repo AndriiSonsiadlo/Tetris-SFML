15027ea76ce886feb4badad30f26a7aedc2c1c79
Andrii Sonsiadlo <andrii.sonsiadlo@gmail.com>
2024-08-05 16:57:48 +0200

    feat: add const Color enumeration

---
+++ src/const/colors.h
@@ -0,0 +1,18 @@
+#pragma once
+
+namespace Tetris
+{
+    enum Color
+    {
+        None = -1,
+        Cyan,
+        Blue,
+        Purple,
+        Pink,
+        LightGreen,
+        Yellow,
+        Red,
+        Green,
+        MaxColors
+    };
+}
