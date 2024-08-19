9c2fe98825a3d863cc32866a8cf5c48e840b1d3b
Andrii Sonsiadlo <andrii.sonsiadlo@gmail.com>
2024-08-19 18:38:14 +0200

    feat: add `getCenterPosition` method to `Tetromino` for calculating geometric center

--- src/include/Tetromino.h
+++ src/include/Tetromino.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cfloat>
 #include <SFML/Graphics.hpp>
 #include <vector>
 
@@ -25,10 +26,31 @@ namespace Tetris
         [[nodiscard]] const std::vector<sf::Vector2i>& getPositions() const { return positions; }
         [[nodiscard]] TileColor getColor() const { return shape.getColor(); }
         [[nodiscard]] TetrominoType getType() const { return shape.getType(); }
+        [[nodiscard]] std::vector<Point> getCoordinates() const { return shape.getCoordinates(); }
 
         void move(int dx, int dy);
         void rotate();
         [[nodiscard]] std::vector<sf::Vector2i> getRotatedPositions() const;
         void setPosition(sf::Vector2i newOffset);
+
+
+        [[nodiscard]] sf::Vector2f getCenterPosition() const
+        {
+            float minX = FLT_MAX, minY = FLT_MAX;
+            float maxX = -FLT_MAX, maxY = -FLT_MAX;
+
+            for (const auto& coord : shape.getCoordinates())
+            {
+                minX = std::min(minX, static_cast<float>(coord.x));
+                minY = std::min(minY, static_cast<float>(coord.y));
+                maxX = std::max(maxX, static_cast<float>(coord.x));
+                maxY = std::max(maxY, static_cast<float>(coord.y));
+            }
+
+            return sf::Vector2f({
+                minX + (maxX - minX + 1) / 2.0f,
+                minY + (maxY - minY + 1) / 2.0f
+            });
+        }
     };
 }
